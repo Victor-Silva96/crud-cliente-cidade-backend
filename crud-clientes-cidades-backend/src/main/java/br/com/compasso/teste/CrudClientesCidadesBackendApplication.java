93aeaf0eaebd2fd3424d2c5a5a6432cebb9f2b13
Silva, Victor Hugo B <victor.hugo.b.silva@accenture.com>
2019-10-31 12:58:17 -0300

    Criando o projeto do teste ja com as dependencias iniciais

---
+++ crud-clientes-cidades-backend/src/main/java/br/com/compasso/teste/CrudClientesCidadesBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.compasso.teste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudClientesCidadesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudClientesCidadesBackendApplication.class, args);
+	}
+
+}
---
+++ crud-clientes-cidades-backend/src/test/java/br/com/compasso/teste/CrudClientesCidadesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.compasso.teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudClientesCidadesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
